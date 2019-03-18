a0d91721fcc4d43997a094b1f831564a9f5e3300
Dmitry Elishevich <delishevich@tula.co>
2019-03-18 23:01:12 +0300

    Improve performance to 60 fps

--- app/src/main/java/ru/ok/technopolis/snowflakes/SnowflakesView.java
+++ app/src/main/java/ru/ok/technopolis/snowflakes/SnowflakesView.java
@@ -20,7 +20,7 @@ public class SnowflakesView extends View {
         super(context, attrs);
         paint = new Paint();
         random = new Random();
-        snows = new Snow[200];
+        snows = new Snow[400];
     }
 
     private Snow[] snows;
@@ -28,30 +28,34 @@ public class SnowflakesView extends View {
     private static int maxHegth;
     private final Paint paint;
     private final Random random;
-    //private Bitmap bitmap;
+    private Bitmap bitmap;
+    private Canvas tmpCanvas;
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         maxWidth = w;
         maxHegth = h;
-        //bitmap = Bitmap.createBitmap(maxWidth, maxHegth, Bitmap.Config.RGB_565);
+        bitmap = Bitmap.createBitmap(maxWidth, maxHegth, Bitmap.Config.ARGB_8888);
+        tmpCanvas = new Canvas(bitmap);
         for (int i = 0; i < snows.length; i++) {
             Snow snow = new Snow();
             reGenerateSnow(random, snow);
             snows[i] = snow;
         }
-        paint.setColor(Color.WHITE & 0x7fffffff);
+        paint.setColor(Color.WHITE & 0xafffffff);
     }
 
     @Override
     protected void onDraw(final Canvas canvas) {
+        bitmap.eraseColor(Color.TRANSPARENT);
         for (Snow snow : snows) {
             snow.invokePosition();
             if (snow.x < 0 || snow.x > maxWidth || snow.y > maxHegth) {
                 reGenerateSnow(random, snow);
             }
-            canvas.drawCircle(snow.x, snow.y, snow.sSize, paint);
+            tmpCanvas.drawCircle(snow.x, snow.y, snow.sSize, paint);
         }
+        canvas.drawBitmap(bitmap, 0, 0, paint);
         invalidate();
     }
 
